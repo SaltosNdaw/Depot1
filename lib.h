8b9ff5f73cb975b89921c060beebba4687c5f74d
Ridjaal <salgattis@gmail.com>
2022-02-03 13:23:47 +0100

    repres_poly_d

--- lib.h
+++ lib.h
@@ -1,2 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+struct init_poly{   
+                    int pow ;
+                    int* coef ;
+                };
+
+typedef struct init_poly poly ;
+typedef struct init_poly* pt_poly ;
--- main.c
+++ main.c
@@ -3,6 +3,21 @@
 
 int main()
 {
+    poly p ;
+    printf("Power =\t") ;
+    scanf("%d", &p.pow) ;
 
+    /*Allocaton dynamique debut*/
+    p.coef = (int*)malloc(p.pow * sizeof(int)) ; 
+    /*Allocaton dynamique fin*/
+ 
+    printf("%d\n", p.pow) ;
+
+    for (int i = 0; i < p.pow; i++)
+    {
+        printf("Coef_%d = ", i+1) ;
+        scanf("%d", p.coef + i) ;
+    }
+    
     return 0 ;
 }
